818b7fde9130455e47dc8cd17436646abf924781
Alan Flores <alanif@hotmail.com>
2025-03-03 12:46:08 -0600

    feat:adding new packages

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,38 @@
+return {
+	'stevearc/conform.nvim',
+	event = { "BufWritePre" },
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<C-S-F>",
+			function()
+				require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+			end
+		}
+	},
+	mode = { "n", "v" },
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+		-- Set default options
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500 },
+		-- Customize formatters
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2" },
+			},
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+}
